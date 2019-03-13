f106f9f781434c7eceb7020fb49229479a818cad
nagaohiroki <testplayers0226@gmail.com>
2019-03-13 14:00:38 +0900

    add game

--- UnityProject/Assets/Scripts/Box.cs
+++ UnityProject/Assets/Scripts/Box.cs
@@ -2,8 +2,27 @@
 public class Box : MonoBehaviour
 {
 	[SerializeField]
-	Rigidbody mRigid;
+	Rigidbody mRigid = null;
+	[SerializeField]
+	Transform mCameraHandle = null;
+	bool mIsPlayer = true;
 	void Update()
 	{
+		if(!mIsPlayer)
+		{
+			return;
+		}
+		mCameraHandle.position = transform.position;
+		if(mRigid == null)
+		{
+			return;
+		}
+		if(Input.GetButtonDown("Fire1"))
+		{
+			var player = Instantiate(this, transform.position + Vector3.up * 0.5f, Quaternion.identity);
+			player.mIsPlayer = true;
+			mIsPlayer = false;
+			mRigid.constraints = RigidbodyConstraints.FreezeAll;
+		}
 	}
 }
