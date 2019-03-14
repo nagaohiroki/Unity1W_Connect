f499dfaf8e03885de0fdee7a09bd641825cb7f84
nagaohiroki <testplayers0226@gmail.com>
2019-03-14 14:00:39 +0900

    modify game

--- UnityProject/Assets/Scripts/Box.cs
+++ UnityProject/Assets/Scripts/Box.cs
@@ -13,16 +13,34 @@ public class Box : MonoBehaviour
 			return;
 		}
 		mCameraHandle.position = transform.position;
+		Shot();
+	}
+	void Shot()
+	{
+		if(!Input.GetButtonDown("Fire1"))
+		{
+			return;
+		}
 		if(mRigid == null)
 		{
 			return;
 		}
-		if(Input.GetButtonDown("Fire1"))
-		{
-			var player = Instantiate(this, transform.position + Vector3.up * 0.5f, Quaternion.identity);
-			player.mIsPlayer = true;
-			mIsPlayer = false;
-			mRigid.constraints = RigidbodyConstraints.FreezeAll;
-		}
+		var vec  = (Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position));
+		vec.z = 0.0f;
+		var player = Instantiate(this, transform.position, Quaternion.identity);
+		player.Player();
+		NotPlayer();
+	}
+	void Player()
+	{
+		var vec  = (Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position));
+		vec.z = 0.0f;
+		mRigid.AddForce(vec.normalized, ForceMode.VelocityChange);
+		mIsPlayer = true;
+	}
+	void NotPlayer()
+	{
+		mRigid.constraints = RigidbodyConstraints.FreezeAll;
+		mIsPlayer = false;
 	}
 }
